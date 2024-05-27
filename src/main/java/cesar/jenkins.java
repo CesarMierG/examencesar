b7e03e622346516b3901d2322f72de1788555c24
Cesar <cesarmiergarcia2014@gmail.com>
2024-05-27 09:41:55 +0200

    prueba previa examen

---
+++ src/main/java/cesar/jenkins.java
@@ -0,0 +1,43 @@
+package cesar;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class jenkins {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jenkins window = new jenkins();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public jenkins() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
